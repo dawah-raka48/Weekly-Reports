f206e7ec616b61149b4e9bac7a49d028d4fc2b35
dawah-raka48 <dawah.raka@gmail.com>
2026-07-26 02:01:54 +0300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -416,25 +416,27 @@ async function saveEmployee(){
 
     });
 
-    if (result.success) {
+        if (result.success) {
 
-    document.getElementById("empName").value = "";
+        document.getElementById("empName").value = "";
 
-    document.getElementById("empUsername").value = "";
+        document.getElementById("empUsername").value = "";
 
-    document.getElementById("empPassword").value = "";
+        document.getElementById("empPassword").value = "";
 
-    document.getElementById("empDepartment").selectedIndex = 0;
+        document.getElementById("empDepartment").selectedIndex = 0;
 
-    document.getElementById("empRole").selectedIndex = 0;
+        document.getElementById("empRole").selectedIndex = 0;
 
-    document
-        .getElementById("employeeModal")
-        .classList
-        .remove("show");
+        document
+            .getElementById("employeeModal")
+            .classList
+            .remove("show");
 
-    employeesPage();
+        employeesPage();
 
-    alert("تمت إضافة الموظف بنجاح");
+        alert("تمت إضافة الموظف بنجاح");
+
+    }
 
 }
