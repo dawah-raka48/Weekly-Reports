c4428660a559b9dd0a4d6b7e61c61ec978ed9293
dawah-raka48 <dawah.raka@gmail.com>
2026-07-26 18:33:53 +0300

    Update employee.js

--- js/employee.js
+++ js/employee.js
@@ -66,3 +66,59 @@ function getWeekNumber(date) {
 
 document.getElementById("weekNumber").textContent =
 "الأسبوع " + getWeekNumber(today);
+
+/* ==========================
+   File Picker
+========================== */
+
+const pdfFile =
+document.getElementById("pdfFile");
+
+const selectedFile =
+document.getElementById("selectedFile");
+
+pdfFile.addEventListener(
+
+    "change",
+
+    ()=>{
+
+        if(!pdfFile.files.length){
+
+            selectedFile.innerHTML=
+
+            "لم يتم اختيار أي ملف";
+
+            return;
+
+        }
+
+        const file=pdfFile.files[0];
+
+        if(file.type!=="application/pdf"){
+
+            alert("يسمح فقط بملفات PDF");
+
+            pdfFile.value="";
+
+            selectedFile.innerHTML=
+
+            "لم يتم اختيار أي ملف";
+
+            return;
+
+        }
+
+        selectedFile.innerHTML=
+
+        file.name+
+
+        "<br>"+
+
+        (file.size/1024/1024).toFixed(2)+
+
+        " MB";
+
+    }
+
+);
