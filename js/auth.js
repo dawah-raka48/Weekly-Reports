ac87b3bb580bff452edaf7b29abcd2041ab6a63c
dawah-raka48 <dawah.raka@gmail.com>
2026-07-26 17:41:15 +0300

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,74 @@
+/* ==========================================
+   Weekly Reports System
+   Authentication
+========================================== */
+
+const loginBtn = document.getElementById("loginBtn");
+const loginText = document.getElementById("loginText");
+
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+
+loginBtn.addEventListener("click", login);
+
+async function login() {
+
+    if (!username.value.trim() || !password.value.trim()) {
+
+        alert("يرجى إدخال اسم المستخدم وكلمة المرور");
+
+        return;
+
+    }
+
+    loginBtn.disabled = true;
+
+    loginText.innerHTML = `
+
+<i class="fa-solid fa-spinner fa-spin"></i>
+
+&nbsp;
+
+جارٍ تسجيل الدخول...
+
+`;
+
+    const result = await api("login", {
+
+        username: username.value.trim(),
+
+        password: password.value.trim()
+
+    });
+
+    if (!result.success) {
+
+        loginBtn.disabled = false;
+
+        loginText.innerHTML = "تسجيل الدخول";
+
+        alert(result.message);
+
+        return;
+
+    }
+
+    localStorage.setItem(
+
+        "currentUser",
+
+        JSON.stringify(result.user)
+
+    );
+
+    if (result.user.role === "admin") {
+
+        location.href = "admin.html";
+
+    } else {
+
+        location.href = "employee.html";
+
+    }
+
+}
