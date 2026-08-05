d5631a404909da3ed0645c1fe07903a5f4a937c1
dawah-raka48 <dawah.raka@gmail.com>
2026-08-05 16:42:04 +0300

    Create manager.js

---
+++ js/manager.js
@@ -0,0 +1,93 @@
+/* ==========================================
+   Weekly Reports System
+   Department Manager
+========================================== */
+
+/* ==========================
+   Current User
+========================== */
+
+const currentUser = JSON.parse(
+    localStorage.getItem("currentUser")
+);
+
+/* ==========================
+   Protection
+========================== */
+
+if (!currentUser) {
+
+    location.href = "index.html";
+
+}
+
+if (currentUser.role !== "manager") {
+
+    location.href = "index.html";
+
+}
+
+/* ==========================
+   Elements
+========================== */
+
+const content =
+document.getElementById("content");
+
+const departmentName =
+document.getElementById("departmentName");
+
+const logoutBtn =
+document.getElementById("logoutBtn");
+
+/* ==========================
+   Department
+========================== */
+
+departmentName.innerHTML =
+
+`قسم ${currentUser.department}`;
+
+/* ==========================
+   Clock
+========================== */
+
+function updateClock(){
+
+    const now = new Date();
+
+    document.getElementById("currentTime").innerHTML =
+
+        now.toLocaleTimeString("ar-SA",{
+
+            hour:"2-digit",
+
+            minute:"2-digit"
+
+        });
+
+    document.getElementById("currentDate").innerHTML =
+
+        now.toLocaleDateString("ar-SA");
+
+}
+
+updateClock();
+
+setInterval(updateClock,1000);
+
+/* ==========================
+   Logout
+========================== */
+
+logoutBtn.onclick=()=>{
+
+    if(confirm("هل تريد تسجيل الخروج؟")){
+
+        localStorage.removeItem("currentUser");
+
+        location.href="index.html";
+
+    }
+
+};
